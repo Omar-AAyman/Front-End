6b34131a4ea7e5da6fd07c977a0466781c621524
Omar <omaraymn411@gmail.com>
2022-06-20 03:10:34 +0200

    edit

---
+++ JS_practicinig/js.js
@@ -0,0 +1,16 @@
+
+//    for (let i = prompt('Please enter your age!',) ;i < 10; i++) {
+//     console.log(i);
+// }
+
+// function sayHi(name,age){
+//     alert(`أهلًا ${name} لقد ولدت سنة ${2022-age} `)
+// }
+// sayHi(prompt('ما اسمك؟',),prompt(' كم عمرك؟ ',));
+
+
+// Arrow Function
+
+
+const ahmed= (name,age) => alert(`أهلًا ${name} لقد ولدت سنة ${2022-age} `)
+ahmed(prompt('ما اسمك؟',),prompt(' كم عمرك؟ ',));
